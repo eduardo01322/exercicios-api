df5fc723a199e58f28cdfce7adcec5d4eafcb852
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:27:31 -0300

    Exercicio 4

--- app/Http/Controllers/ExQuatro.php
+++ app/Http/Controllers/ExQuatro.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ExQuatro extends Controller
 {
-    //
+    public function verificar(request $request){
+        if($request->numero % 2 ==0){
+            return json_encode([
+                'mensagem' => 'par'
+            ]);
+        } else {
+            return json_encode([
+                'mensagem' => 'impar'
+            ]);
+        }
+    }
 }
