3528d372d87b270e7970cac6ca8a195ec61c4f03
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:28:08 -0300

    Exercicio 8

--- app/Http/Controllers/ExOito.php
+++ app/Http/Controllers/ExOito.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ExOito extends Controller
 {
-    //
+    public function tabuada(request $request){
+
+        $numero = $request->numero_tabuada;
+        $numeros = [];
+        
+        for ($i = 0; $i <= 10; $i++) {
+            $calculo = $numero * $i;
+            $resultado = $numero . " x " . $i . " = " . $calculo;
+            array_push($numeros, $resultado);
+        }
+        return json_encode([
+            "resultado" => $numeros
+        ]);
+        }
 }
