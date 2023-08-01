fbcc55945ee36c4616263e40dd2ca038b8d5d749
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:27:01 -0300

    Exercicio 5

--- app/Http/Controllers/ExCinco.php
+++ app/Http/Controllers/ExCinco.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ExCinco extends Controller
 {
-    //
+    public function divisivel(request $request){
+        if($request->numero % 4 ==0){
+            return json_encode([
+                'mensagem' => 'divisivel por 4'
+            ]);
+        } else {
+            return json_encode([
+                'mensagem' => 'não é divisivel por 4'
+            ]);
+        }
+    }
 }
