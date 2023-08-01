7298e9d977a982736daa471e1eadbcba823f4589
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:28:24 -0300

    Exercicio 10

--- app/Http/Controllers/ExDez.php
+++ app/Http/Controllers/ExDez.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ExDez extends Controller
 {
-    //
+    public function impar()
+    {
+        $numeros = []; 
+
+        for ($i = 0; $i <= 80; $i++) {
+
+        if ($i % 2 ==1)  array_push($numeros, $i);
+        
+        }
+
+        return json_encode([
+            'valores'=> $numeros
+        ]);
+    }
 }
