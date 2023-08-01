c223320c287b6014c91d9a2203b69f44b0dac887
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:27:22 -0300

    Exercicio 3

--- app/Http/Controllers/ExTres.php
+++ app/Http/Controllers/ExTres.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ExTres extends Controller
 {
-    //
+    public function media(Request $request){
+        $primeiraEntrada = $request->primeiro_numero;
+        $segundaEntrada = $request->segundo_numero;
+        $terceiraEntrada = $request->terceiro_numero;
+
+        return json_encode([
+            'resultado' => ($primeiraEntrada + $segundaEntrada + $terceiraEntrada) / 3
+        ]); 
+    }
 }
