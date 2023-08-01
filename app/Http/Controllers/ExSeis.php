6ec6db54a583ec56a8f73ae93ae589edcb2b1ffe
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-01 10:27:42 -0300

    Exercicio 6

--- app/Http/Controllers/ExSeis.php
+++ app/Http/Controllers/ExSeis.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class ExSeis extends Controller
 {
-    //
+    public function idade(request $request){
+        if($request->numero <= 12){
+            return json_encode([
+                'mensagem' => 'É uma criança'
+            ]);
+        } else if($request->numero > 18){
+            return json_encode([
+                'mensagem' => 'É um adulto'
+            ]);
+        } else {
+            return json_encode([
+                'mensagem' => 'é um adolescente'
+            ]);
+        }
+    }
 }
