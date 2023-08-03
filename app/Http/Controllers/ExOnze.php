e5e8cfd883947ec80c49c0cccf84fad9dd18b7e9
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-03 12:21:15 -0300

    .

--- app/Http/Controllers/ExOnze.php
+++ app/Http/Controllers/ExOnze.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class ExOnze extends Controller
 {
-    //
+    public function tabuada(request $request){
+
+        $n1 = $request->primeiro_numero;
+        $n2 = $request->segundo_numero;
+        
+
+        if($request->operação){
+            return json_encode([
+                'resultado' => $n1 * $n2
+            ]);
+    } else if($request->operação){
+        return json_encode([
+            'resultado' => $n1 + $n2
+        ]);
+    } else if($request->operação){
+        return json_encode([
+            'resultado' => $n1 - $n2
+        ]);
+     } else if($request->operação){
+            return json_encode([
+                'resultado' => $n1 / $n2
+            ]);
+    } } 
 }
+
